519cd52527e996a35eef5576c02581424cb360f5
Bryce Kennedy <btken88@gmail.com>
2020-05-02 18:07:26 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,64 @@
+require 'pry'
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  return card_number = rand(1..11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  user_choice = gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  # deal 2 cards to the user's hand
+  hand = deal_card + deal_card
+  # show user hand points
+  display_card_total(hand)
+  return hand
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  card_total = total
+  # prompt user and store user's choice
+  prompt_user
+  choice = get_user_input
+  until choice == 's' || choice == 'h' do
+    invalid_command
+    prompt_user
+    choice = get_user_input
+  end
+  if choice == 's'
+    return card_total
+  end
+  if choice == 'h'
+    card_total += deal_card
+    return card_total
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +66,24 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  user_hand = initial_round
+  prompt_user
+  hit_or_stay = get_user_input
+  if hit_or_stay == 's'
+    prompt_user
+    hit_or_stay = get_user_input
+    if hit_or_stay == 's'
+      return display_card_total(user_hand)
+    end
+  end
+  if hit_or_stay == 'h'
+    new_card = deal_card
+    user_hand += new_card
+    display_card_total(user_hand)
+  end
+  until user_hand > 21
+    user_hand = hit?(user_hand)
+  end
+  end_game(user_hand)
 end
-    
